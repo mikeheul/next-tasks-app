645924154c59e986e57cbfbaadde8abeb06a3abc
Micka <mickael.murmann@gmail.com>
2024-03-12 14:54:44 +0100

    set up context providers

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,15 @@
 "use client";
 import React from 'react'
+import styled from 'styled-components';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <SidebarStyled>Sidebar</SidebarStyled>
   )
 }
+
+const SidebarStyled = styled.nav`
+    
+`;
 
 export default Sidebar
