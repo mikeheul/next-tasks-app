529d233687442e242da40d76f576047726f93bb6
Micka <mickael.murmann@gmail.com>
2024-03-12 17:34:00 +0100

    update profile section of sidebar

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -6,15 +6,16 @@ import Image from 'next/image';
 
 import menu from "@/app/utils/menu"
 import Link from 'next/link';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 const Sidebar = () => {
 
   const { theme } = useGlobalState();
 
   const router = useRouter();
+  const pathName = usePathname();
 
-  const handleClient = (link:string) => {
+  const handleClick = (link:string) => {
     router.push(link)
   };
 
@@ -26,18 +27,29 @@ const Sidebar = () => {
           <Image width={70} height={70} src="/avatar1.png" alt="profile" />
         </div>
         <h1>
-          <span>Micka</span>
-          <span>Murmann</span>
+          <span>My</span>
+          <span>User</span>
         </h1>
       </div>
       <ul className="nav-items">
         {menu.map((item) => {
-          return <li>
-            {item.icon}
-            <Link href={item.link}>{item.title}</Link>
-          </li>
+
+          const link = item.link;
+
+          return (
+            <li 
+              className={`nav-item ${ pathName === link ? "active" : "" }`} 
+              onClick={() => {
+                handleClick(link);
+              }}
+            >
+              {item.icon}
+              <Link href={link}>{item.title}</Link>
+            </li>
+          );
         })}
       </ul>
+      <button></button>
     </SidebarStyled>
   )
 }
@@ -49,6 +61,37 @@ const SidebarStyled = styled.nav`
     background-color: ${(props) => props.theme.colorBg2};
     border: 2px solid ${(props) => props.theme.borderColor2};
     border-radius: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    color: ${(props) => props.theme.colorGrey3};
+
+    .profile {
+      margin: 1.5rem;
+      padding: 1rem 0.8rem;
+      position: relative;
+      border-radius: 1rem;
+      cursor: pointer;
+      font-weight: 500;
+      color: ${(props) => props.theme.colorGrey0};
+      display: flex;
+      align-items: center;
+    }
+
+    .profile-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      backdrop-filter: blur(5px);
+      z-index: 0;
+      background: ${(props) => props.theme.colorBg3};
+      transition: all 0.55s linear;
+      border-radius: 1rem;
+      border: 2px solid ${(props) => props.theme.borderColor2};
+      opacity: 0.2;
+    }
 `;
 
 export default Sidebar
