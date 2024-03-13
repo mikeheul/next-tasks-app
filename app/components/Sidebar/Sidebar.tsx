dc402d063f50e4f6c5f11441de8e348969329277
Micka <mickael.murmann@gmail.com>
2024-03-13 11:34:03 +0100

    update Sidebar style : active link

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -67,16 +67,18 @@ const SidebarStyled = styled.nav`
     color: ${(props) => props.theme.colorGrey3};
 
     .profile {
-      margin: 1.5rem;
-      padding: 1rem 0.8rem;
-      position: relative;
-      border-radius: 1rem;
-      cursor: pointer;
-      font-weight: 500;
-      color: ${(props) => props.theme.colorGrey0};
-      display: flex;
-      align-items: center;
-    }
+    margin: 1.5rem;
+    padding: 1rem 0.8rem;
+    position: relative;
+
+    border-radius: 1rem;
+    cursor: pointer;
+
+    font-weight: 500;
+    color: ${(props) => props.theme.colorGrey0};
+
+    display: flex;
+    align-items: center;
 
     .profile-overlay {
       position: absolute;
@@ -84,12 +86,13 @@ const SidebarStyled = styled.nav`
       left: 0;
       width: 100%;
       height: 100%;
-      backdrop-filter: blur(5px);
+      backdrop-filter: blur(10px);
       z-index: 0;
       background: ${(props) => props.theme.colorBg3};
       transition: all 0.55s linear;
       border-radius: 1rem;
       border: 2px solid ${(props) => props.theme.borderColor2};
+
       opacity: 0.2;
     }
 
@@ -97,10 +100,12 @@ const SidebarStyled = styled.nav`
       font-size: 1.2rem;
       display: flex;
       flex-direction: column;
+
       line-height: 1.4rem;
     }
 
-    .image, h1 {
+    .image,
+    h1 {
       position: relative;
       z-index: 1;
     }
@@ -110,7 +115,8 @@ const SidebarStyled = styled.nav`
       display: inline-block;
       overflow: hidden;
       transition: all 0.5s ease;
-      border-radius: 50%;
+      border-radius: 100%;
+
       width: 70px;
       height: 70px;
 
@@ -121,21 +127,94 @@ const SidebarStyled = styled.nav`
     }
 
     > h1 {
-      margin-left: 0.8;
+      margin-left: 0.8rem;
       font-size: clamp(1.2rem, 4vw, 1.4rem);
       line-height: 100%;
     }
 
-    .profile:hover {
+    &:hover {
       .profile-overlay {
         opacity: 1;
         border: 2px solid ${(props) => props.theme.borderColor2};
       }
 
       img {
-        transform: scale(1.1)
+        transform: scale(1.1);
       }
     }
+  }
+
+  .nav-item {
+    position: relative;
+    padding: 0.8rem 1rem 0.9rem 2.1rem;
+    margin: 0.3rem 0;
+
+    display: grid;
+    grid-template-columns: 40px 1fr;
+    cursor: pointer;
+    align-items: center;
+
+    &::after {
+      position: absolute;
+      content: "";
+      left: 0;
+      top: 0;
+      width: 0;
+      height: 100%;
+      background-color: ${(props) => props.theme.activeNavLinkHover};
+      z-index: 1;
+      transition: all 0.3s ease-in-out;
+    }
+
+    &::before {
+      position: absolute;
+      content: "";
+      right: 0;
+      top: 0;
+      width: 0%;
+      height: 100%;
+      background-color: ${(props) => props.theme.colorGreenDark};
+
+      border-bottom-left-radius: 5px;
+      border-top-left-radius: 5px;
+    }
+
+    a {
+      font-weight: 500;
+      transition: all 0.3s ease-in-out;
+      z-index: 2;
+      line-height: 0;
+    }
+
+    i {
+      display: flex;
+      align-items: center;
+      color: ${(props) => props.theme.colorIcons};
+    }
+
+    &:hover {
+      &::after {
+        width: 100%;
+      }
+    }
+  }
+
+  .active {
+    background-color: ${(props) => props.theme.activeNavLink};
+
+    i,
+    a {
+      color: ${(props) => props.theme.colorIcons2};
+    }
+  }
+
+  .active::before {
+    width: 0.3rem;
+  }
+
+  > button {
+    margin: 1.5rem;
+  }
 `;
 
 export default Sidebar
